36e838c7a0b30dbb7fff1f39319d475d37a3ef38
nikhilfurious <aashishchandak1999@gmail.com>
2019-04-11 20:54:56 +0530

    Update duplicate-number.java

--- duplicate-number.java
+++ duplicate-number.java
@@ -5,10 +5,10 @@ public class Main {
     {
        int n,temp, sum = 0;
         Scanner s = new Scanner(System.in);
-       System.out.print("Enter no. of elements you want in array:");
+       //System.out.print("Enter no. of elements you want in array:");
         n = s.nextInt();
         int a[] = new int[n];
-        System.out.println("Enter all the elements:");
+        //System.out.println("Enter all the elements:");
         for(int i = 0; i < n; i++)
         {
             a[i] = s.nextInt();
